a735c5f07e5bfb174836e556e4f5ee70a12a0318
sAnKeT-nAiK <1100sanket@gmail.com>
2018-09-21 20:15:29 +0530

    hello

--- Bookmela/settings.py
+++ Bookmela/settings.py
@@ -119,7 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' 
+# if DEBUG:
+#   MEDIA_URL = '/media/'
+#   STATIC_ROOT=os.path.join(os.path.dirname(BASE_DIR),"static","static-only")
+#   MEDIA_ROOT=os.path.join(os.path.dirname(BASE_DIR),"static","media")
 
 STATICFILES_DIRS=(
     os.path.join(BASE_DIR, 'static'),
